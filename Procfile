69126109c3b361053eb45161452ee5023077b979
Seagyn <seagyn@digitalleap.co.za>
2016-09-29 22:02:59 +0200

    Remove index for now

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
 web: node wp-devhub-bot.js
-web: node index.js
--- index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-
-app.listen(80, function () {
-    response.writeHead(302, {
-      'Location': 'https://leapsandbounds.io'
-    });
-    response.end();
-});
